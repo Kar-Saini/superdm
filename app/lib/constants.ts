c282e22e85ea2d610d3d1002e8e33a7346d391ae
Kartik Saini <kackysaini8@gmail.com>
2026-03-18 18:10:25 +0530

    moved contract

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,6 +1,6 @@
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 
-import idl from "../../contract/target/idl/contract.json";
+import idl from "../contract/target/idl/contract.json";
 
 export const PROGRAM_ID = new PublicKey(idl.address);
 export const connection = new Connection(clusterApiUrl("devnet"));
